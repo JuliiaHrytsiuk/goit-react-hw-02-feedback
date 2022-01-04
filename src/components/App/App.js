b1cd94cdcc463b4a56e30970c788bbf0704cfa9e
JuliiaHrytsiuk <tuuspoliglotus@gmail.com>
2022-01-04 20:23:07 +0200

    ..

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import Feedback from "../Feedback/Feedback";
+
+import { Container } from "./App.styled";
+
+export default function App() {
+  return (
+    <Container>
+      <Feedback />
+    </Container>
+  );
+}
